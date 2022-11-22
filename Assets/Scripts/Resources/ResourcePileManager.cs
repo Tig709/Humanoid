eb62c515e820369224bcb33309fd4e476023281a
Tigo <tigo.stam@hva.nl>
2022-11-22 23:35:04 +0100

    Resource script working, resource system cleaning by removing unnecesary shit

--- Assets/Scripts/Resources/ResourcePileManager.cs
+++ Assets/Scripts/Resources/ResourcePileManager.cs
@@ -5,104 +5,75 @@ using UnityEngine.UI;
 
 public class ResourcePileManager : MonoBehaviour
 {
-
-    [SerializeField]
-    private GameObject ScrapLayer1;
-    [SerializeField]
-    private GameObject ScrapLayer2;
-    [SerializeField]
-    private GameObject ScrapLayer3;
-    [SerializeField]
-    private GameObject ScrapLayer4;
-    [SerializeField]
-    private GameObject ScrapLayer5;
-
-    [SerializeField]
+    //current time used to count the cooldown
+    [SerializeField]//serialized for debugging
     private int currentTime = 0;
+
+    //cooldown is used to leave a resource spot empty after all resources are taken and when game starts
     [SerializeField]
-    private int startTime = 15;
+    private int cooldown = 5;
+
+    //time between next is used to determine the time between the next upgrade of size for the resource pile
     [SerializeField]
-    private int timeBetweenNext = 1;
+    private int timeBetweenNext = 3;
+
+    //level is the amount of resources you get from the spot
+    [SerializeField]//serialized for debugging
+    private int pileLevel = 0;
+
+    //size when pile stops growing
     [SerializeField]
-    private int heapLevel = 0;
+    private int maxPileLevel = 5;
 
-    private int starttime = 0;
-    private bool heapLevel0 = true;
+    //this boolean checks if pilelevel is zero
+    private bool isPileLevel0 = true;
 
-
-
-    // Start is called before the first frame update
+    private GameObject Scrap;
+    float timePassed = 0f;
+    
     void Start()
     {
+        Scrap = this.gameObject;
         StartCounting();
     }
 
     private void Update()
     {
-        Debug.Log("Resource pile level is " + heapLevel);
-        Debug.Log("Current time is " + currentTime);
-
-        switch (heapLevel)
+        Debug.Log(pileLevel);
+        if (!isPileLevel0 && pileLevel <= maxPileLevel)
         {
-            case 0:
-                break;
-            case 1: 
-                Destroy(this.gameObject);
-                Instantiate(ScrapLayer1, transform.position, Quaternion.identity);
-                break;
-            case 2:
-                Destroy(this.gameObject);
-                Instantiate(ScrapLayer2, transform.position, Quaternion.identity);
-                break;
-            case 3:
-                Destroy(this.gameObject);
-                Instantiate(ScrapLayer3, transform.position, Quaternion.identity);
-                break;
-            case 4:
-                Destroy(this.gameObject);
-                Instantiate(ScrapLayer4, transform.position, Quaternion.identity);
-                break;
-            case 5:
-                Destroy(this.gameObject);
-                Instantiate(ScrapLayer5, transform.position, Quaternion.identity);
-                break;
+            timePassed += Time.deltaTime;
+            if (timePassed > timeBetweenNext)
+            {
+                pileLevel++;
+                Scrap.transform.localScale *= 1.2f;
+                timePassed = 0;
+            }
         }
     }
 
     public void StartCounting()
     {
-
-        InvokeRepeating("Count", 0, 1);
-
+        if (isPileLevel0)
+        {
+            InvokeRepeating("Count", 0, 1);
+        }
     }
 
     public void Count()
     {
-        if (currentTime < startTime)
+        if (currentTime < cooldown)
         {
             currentTime++;
         }
 
-        if (currentTime == startTime && heapLevel0)
+        if (currentTime == cooldown && isPileLevel0)
         {
-            heapLevel = 1;
-            heapLevel0 = false; 
-            currentTime = 0;
-        }
-    }
-
-    public void nextHeapLevel()
-    {
-
-        if (heapLevel0 && starttime < timeBetweenNext)
-        {
-            starttime++;
-        }
-        if (currentTime >= timeBetweenNext && !heapLevel0)
-        {
-            currentTime = 0;
-            heapLevel++;
+            pileLevel++;
+            Scrap.transform.localScale *= 1.2f;
+            isPileLevel0 = false;
         }
     }
 }
+
 
